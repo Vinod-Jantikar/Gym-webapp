f8206fab5d9f3337f93809ecf1b2d7a61c6d9565
Vinod-Jantikar <vinodjantikar999@gmail.com>
2023-08-30 00:29:58 +0530

    added navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import Navbar from "@/pages/navbar";
+import { SelectedPage } from "@/shared/types";
+
 function App() {
+  const [selectedPage, setSelectedPage] = React.useState<SelectedPage>(SelectedPage.Home);
   return (
-    <div className="app">
-App
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   )
 }
